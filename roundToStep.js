5e74472eaa44d6b9a2d1e0eb409f25e1f52532bb
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-29 14:29:50 -0400

    feat: remove all dependencies

--- roundToStep.js
+++ roundToStep.js
@@ -1,13 +1,22 @@
-
-var countDecimals = require('countDecimals');
-var roundValue = require('roundValue');
-
-function roundToStep(x, step) {
-  var decs = countDecimals(step);
-  if (decs) {
-    return roundValue(Math.round(x / step) * step, decs);
+function roundToStep(n, step) {
+  // Calculate the number of decimals in our step.
+  let p = 0
+  if (step % 1) {
+    const s = String(step)
+    p = s.length - s.indexOf('.') - 1
   }
-  return Math.round(x / step) * step;
+
+  // Perform naïve rounding.
+  n = Math.round(n / step) * step
+
+  // Work some magic to prevent inexact decimals.
+  // eg: roundToStep(100.6, 0.015) => 100.6 (instead of 100.60499999999999)
+  if (p) {
+    const q = Math.pow(10, p)
+    return Math.round(q * n + q / 1e16) / q
+  }
+
+  return n
 }
 
-module.exports = roundToStep;
+module.exports = roundToStep
